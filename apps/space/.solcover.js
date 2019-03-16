a1d6aea751e8858d9c9345d7c48dddea9648f4b8
Haythem Sellami <sellamihaythem96@gmail.com>
2019-03-16 03:58:18 +0100

    code coverage

---
+++ apps/space/.solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+    norpc: true,
+    copyPackages: ['@aragon/os'],
+    buildDirPath: '/build/contracts',
+    skipFiles: [
+        'Migrations.sol',
+        'test/Spoof.sol'
+    ]
+}

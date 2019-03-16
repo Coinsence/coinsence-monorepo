ccf14588d631af637509094ff18acdff10d824e4
Haythem Sellami <sellamihaythem96@gmail.com>
2019-03-16 23:16:05 +0100

    fix coverage with npm run coverage:space

--- apps/space/.solcover.js
+++ apps/space/.solcover.js
@@ -2,6 +2,7 @@ module.exports = {
     norpc: true,
     copyPackages: ['@aragon/os'],
     buildDirPath: '/build/contracts',
+    testCommand: '../node_modules/.bin/aragon contracts test --network coverage',
     skipFiles: [
         'misc/Migrations.sol',
         'test/Spoof.sol',

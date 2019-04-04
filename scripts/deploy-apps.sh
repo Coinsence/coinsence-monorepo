eeff960770b8e653807857fd314115233ed82fc7
Haythem Sellami <sellamihaythem96@gmail.com>
2019-04-04 15:43:36 +0100

    modify deploy apps script

--- scripts/deploy-apps.sh
+++ scripts/deploy-apps.sh
@@ -11,11 +11,14 @@ echo "a new app version will be deployed"
 echo "----"
 
 for dir in ./apps/*/; do
-  set -x
-  cd $dir
-  aragon apm publish major 
-  cd $rootDir
-  set +x
+  if [ $dir != "./apps/coinsence-kit/" -a $dir != "./apps/shared/" ]
+  then
+    set -x
+    cd $dir
+    aragon apm publish major 
+    cd $rootDir
+    set +x
+  fi
 done
 
 echo "Done, new versions of all apps deployed"
